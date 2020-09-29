a74eec3b9f52e20ce063c0dd1e91ebf541a4b542
Vincent Ivos <vincentivos1@gmail.com>
2020-09-29 19:51:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,7 +188,7 @@ end
 def player_numbers(team_name)
   a = []
   game_hash.each do|key1, value1|
-  value1.each do |key2| 
+  value1.each do |key2, value2| 
   if key2 == :team_name
   key2.each do |key3, value3|
   if key3 == :number   
